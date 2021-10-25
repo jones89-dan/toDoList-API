55c869addeaa0e204b33dea649047d26cf531eb4
Daniel <jones.89.dan@gmail.com>
2021-10-24 18:25:58 -0700

    Added the POST Ajax function to add new items

--- application.js
+++ application.js
@@ -1,15 +1,45 @@
 $(document).ready(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=169',
-    dataType: 'json',
-    success: function (response, textStatus) {
-      response.tasks.forEach(function (task) {
-        $('#todo-list').append('<p>' + task.content + '</p>');
-      })
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
+  var getAndDisplayAllTasks = function () {
+    $.ajax({
+      type: 'GET',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=169',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        $('#todo-list').empty();
+        response.tasks.forEach(function (task) {
+          $('#todo-list').append('<p>' + task.content + '</p>');
+        })
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=169',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
   });
+
+  getAndDisplayAllTasks();
 });
