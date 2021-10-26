bcee4db82f13f36f6ec78ed1bcbed08158cac711
Daniel <jones.89.dan@gmail.com>
2021-10-25 20:15:33 -0700

    Crated the mark complete and the the mark active functions

--- application.js
+++ application.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
           $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content +
-          '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
+          '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="'
+          + task.id + '"' + (task.completed ? 'checked' : '') + '>');
         })
       },
       error: function (request, textStatus, errorMessage) {
@@ -56,9 +57,49 @@ var deleteTask = function (id) {
       });
 }
 
-$(document).on('click', '.delete', function () {
-    deleteTask($(this).data('id'));
+  $(document).on('click', '.delete', function () {
+      deleteTask($(this).data('id'));
+    });
+
+    var markTaskComplete = function (id) {
+      $.ajax({
+        type: 'PUT',
+        url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=169',
+        dataType: 'json',
+        success: function (response, textStatus) {
+          console.log(response);
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+  }
+
+  var markTaskActive = function (id) {
+  $.ajax({
+ type: 'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=169',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      getAndDisplayAllTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
   });
+}
+
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+      markTaskComplete($(this).data('id'));
+    }
+    else {
+      markTaskActive($(this).data('id'));
+    }
+  });
+
+
+
 
   getAndDisplayAllTasks();
 });
